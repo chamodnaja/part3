db996015b814b07aebfb6019f9d66f789ba5ff16
chamodnaja <39210152+chamodnaja@users.noreply.github.com>
2021-08-30 09:24:47 +0700

    Save 1

--- screens/evaluation.js
+++ screens/evaluation.js
@@ -6,17 +6,39 @@ export default function Profile({route}) {
         <ScrollView>
             <View style={styles.container}>
                 <Text>คำชี้แจง: ทำแบบประเมินดังต่อไปนี้ให้ครบทุกข้อ</Text>
-                
+                <RadioForm formHorizontal={false} >
+                    <View style={styles.itemChoice}>
+                        <RadioButton>
+                            <RadioButtonInput 
+                                isSelected={true}
+                                buttonSize={14}
+                                buttonInnerColor={'red'}
+                                buttonOuterColor={'blue'}
+                                
+                            />
+                        </RadioButton>
+                        <Text style={styles.textChoice}>ตัวเลือกที่ 1</Text>
+                    </View>
+                </RadioForm>
             </View>
         </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
+    textChoice:{
+       marginLeft: 6 
+    },
+    itemChoice: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-start'
+    },
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
+        alignItems: 'flex-start',
         justifyContent: 'flex-start',
+        paddingLeft: 8,
     },
 });
